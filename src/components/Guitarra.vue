f5147b1607648c42ec24e3420cac42dbbbb6ae98
CarolineT9 <caroltvrs@gmail.com>
2024-09-03 15:48:56 -0300

    guitarLa

--- src/components/Guitarra.vue
+++ src/components/Guitarra.vue
@@ -7,18 +7,25 @@
             <h3 class="text-black fs-4 fw-bold text-uppercase">{{ guitarra.nombre }}</h3>
             <p>{{ guitarra.descripcion }}</p>
             <p class="fw-black text-primary fs-3">{{ guitarra.precio }}</p>
-            <button type="button" class="btn btn-dark w-100 ">Agregar al Carrito</button>
+            <button type="button" class="btn btn-dark w-100 " @click="$emit('add-car', guitarra)">Agregar al Carrito</button>
         </div>
     </div>
 </template>
 
 <script setup>
+
 const props = defineProps({
     guitarra:{
         type: Object,
         required: true
     }
 })
+
+
+
+
+defineEmits('add-car')
+
 </script>
 
 <style></style>
